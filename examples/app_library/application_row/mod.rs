b23ec4976f89765caee5276d8ced3e15c8d8b76c
Ashley Wulber <ashley@system76.com>
2021-12-02 12:49:01 -0500

    feat: basic application library grid view

---
+++ examples/app_library/application_row/mod.rs
@@ -0,0 +1,31 @@
+use gtk4 as gtk;
+mod imp;
+
+use gtk::prelude::*;
+use gtk::subclass::prelude::*;
+use gtk::{gio, glib};
+
+glib::wrapper! {
+    pub struct ApplicationRow(ObjectSubclass<imp::ApplicationRow>)
+        @extends gtk::Widget, gtk::Box;
+}
+
+impl Default for ApplicationRow {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl ApplicationRow {
+    pub fn new() -> Self {
+        glib::Object::new(&[]).expect("Failed to create ApplicationRow")
+    }
+
+    pub fn set_app_info(&self, app_info: &gio::AppInfo) {
+        let self_ = imp::ApplicationRow::from_instance(self);
+        self_.name.set_text(&app_info.name());
+        if let Some(icon) = app_info.icon() {
+            self_.image.set_from_gicon(&icon);
+        }
+    }
+}
---
+++ examples/app_library/main.rs
@@ -0,0 +1,34 @@
+mod application_row;
+mod window;
+
+use gtk::gdk::Display;
+use gtk::prelude::*;
+use gtk4 as gtk;
+
+use window::Window;
+
+fn main() {
+    let application = gtk::Application::new(
+        Some("com.github.gtk-rs.examples.apps_launcher"),
+        Default::default(),
+    );
+
+    application.connect_activate(|app| {
+        let provider = gtk::CssProvider::new();
+        provider.load_from_data(include_bytes!("style.css"));
+        gtk::StyleContext::add_provider_for_display(
+            &Display::default().expect("Error initializing gtk css provider."),
+            &provider,
+            gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+        );
+        new_build_ui(app);
+    });
+
+    application.run();
+}
+
+fn new_build_ui(app: &gtk::Application) {
+    // Create a new custom window and show it
+    let window = Window::new(app);
+    window.show();
+}
