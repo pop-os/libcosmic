4ae03278ccd95cb90311bf99fec610106c92cb0e
Eduardo Flores <edfloreshz@gmail.com>
2022-09-30 18:43:32 -0700

    Fixed sidebar responsiveness
    - Created new widget `scrollbar` which can adopt COSMIC styling.
    - Increased sidebar icon size by 4 pixels to improve visibility.
    - Added padding inside the navigation bar to prevent collision with the scrollbar.
    - Increased the max width of the condensed navbar for the scrollbar.

--- src/widget/nav.rs
+++ src/widget/nav.rs
@@ -9,12 +9,14 @@ use iced::{
 macro_rules! nav_bar {
     ($($x:expr),+ $(,)?) => (
         $crate::iced::widget::Container::new(
-            $crate::iced::widget::scrollable(
-                $crate::iced::widget::Column::with_children(
-                    vec![$($crate::iced::Element::from($x)),+]
-                )
-                .spacing(12)
-                .padding([0,20,0,0])
+            $crate::scrollbar!(
+                $crate::iced::widget::row![
+                    $crate::iced::widget::Column::with_children(
+                        vec![$($crate::iced::Element::from($x)),+]
+                    )
+                    .spacing(12)
+                    .padding(12),
+                ]
             )
         )
         .max_width(300)
@@ -43,12 +45,12 @@ macro_rules! nav_button {
     ($icon: expr, $title:expr, $condensed:expr) => ({
         if $condensed {
             $crate::iced::widget::Button::new(
-                $crate::widget::icon($icon, 16)
+                $crate::widget::icon($icon, 20)
             )
             .padding(8)
         } else {
             $crate::widget::button!(
-                $crate::widget::icon($icon, 16),
+                $crate::widget::icon($icon, 20),
                 $crate::iced::widget::Text::new($title),
                 $crate::iced::widget::horizontal_space(
                     $crate::iced::Length::Fill
