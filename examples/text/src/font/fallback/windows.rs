51e3ef662701cdb02c30dc19bf0a3b6c3f992e93
Jeremy Soller <jackpot51@gmail.com>
2022-10-13 13:38:06 -0600

    Add some windows common fallbacks

--- examples/text/src/font/fallback/windows.rs
+++ examples/text/src/font/fallback/windows.rs
@@ -2,10 +2,18 @@ use unicode_script::Script;
 
 // Fallbacks to use after any script specific fallbacks
 pub fn common_fallback() -> &'static [&'static str] {
-    &[]
+    //TODO: abstract style (sans/serif/monospaced)
+    &[
+        "Segoe UI",
+        "FreeSans",
+        "Segoe UI Symbol",
+        "Segoe UI Emoji",
+        //TODO: Add CJK script here for doublewides?
+    ]
 }
 
 // Fallbacks to use per script
 pub fn script_fallback(script: &Script, locale: &str) -> &'static [&'static str] {
+    //TODO: per-script fallbacks
     &[]
 }
