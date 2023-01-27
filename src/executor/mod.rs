710f8dfd4a4cc36579a39ab811af2a9490539e9d
Michael Aaron Murphy <michael@mmurphy.dev>
2023-01-27 03:58:39 +0100

    feat(executor): add multi::Executor

--- src/executor/mod.rs
+++ src/executor/mod.rs
@@ -1,3 +1,10 @@
+// Copyright 2023 System76 <info@system76.com>
+// SPDX-License-Identifier: MPL-2.0
+
+#[cfg(feature = "tokio")]
+pub mod multi;
+
+#[cfg(feature = "tokio")]
 pub mod single;
 
 #[cfg(not(feature = "tokio"))]
