d15999e5db1f53ecb7f536dff46318cc66bdee4d
Ashley Wulber <ashley@system76.com>
2023-08-15 17:17:16 -0400

    wip: text input

---
+++ src/widget/text_input/mod.rs
@@ -0,0 +1,9 @@
+//! A text input widget from iced_widgets plus some added details.
+
+pub mod cursor;
+pub mod editor;
+pub mod style;
+mod text_input;
+pub mod value;
+
+pub use text_input::*;
---
+++ src/widget/text_input/style.rs
@@ -0,0 +1,259 @@
+//! Change the appearance of a text input.
+use iced_core::{Background, BorderRadius, Color};
+
+/// The appearance of a text input.
+#[derive(Debug, Clone, Copy)]
+pub struct Appearance {
+    /// The [`Background`] of the text input.
+    pub background: Background,
+    /// The border radius of the text input.
+    pub border_radius: BorderRadius,
+    /// The border offset
+    pub border_offset: Option<f32>,
+    /// The border width of the text input.
+    pub border_width: f32,
+    /// The border [`Color`] of the text input.
+    pub border_color: Color,
+    /// The icon [`Color`] of the text input.
+    pub icon_color: Color,
+    /// The label [`Color`] of the text input.
+    pub label_color: Color,
+}
+
+/// A set of rules that dictate the style of a text input.
+pub trait StyleSheet {
+    /// The supported style of the [`StyleSheet`].
+    type Style: Default;
+
+    /// Produces the style of an active text input.
+    fn active(&self, style: &Self::Style) -> Appearance;
+
+    /// Produces the style of an errored text input.
+    fn error(&self, style: &Self::Style) -> Appearance;
+
+    /// Produces the style of a focused text input.
+    fn focused(&self, style: &Self::Style) -> Appearance;
+
+    /// Produces the [`Color`] of the placeholder of a text input.
+    fn placeholder_color(&self, style: &Self::Style) -> Color;
+
+    /// Produces the [`Color`] of the value of a text input.
+    fn value_color(&self, style: &Self::Style) -> Color;
+
+    /// Produces the [`Color`] of the value of a disabled text input.
+    fn disabled_color(&self, style: &Self::Style) -> Color;
+
+    /// Produces the [`Color`] of the selection of a text input.
+    fn selection_color(&self, style: &Self::Style) -> Color;
+
+    /// Produces the style of an hovered text input.
+    fn hovered(&self, style: &Self::Style) -> Appearance {
+        self.focused(style)
+    }
+
+    /// Produces the style of a disabled text input.
+    fn disabled(&self, style: &Self::Style) -> Appearance;
+}
+
+#[derive(Copy, Clone, Default)]
+pub enum TextInput {
+    #[default]
+    Default,
+    ExpandableSearch,
+    Search,
+    Inline,
+}
+
+impl StyleSheet for crate::Theme {
+    type Style = TextInput;
+
+    fn active(&self, style: &Self::Style) -> Appearance {
+        let palette = self.cosmic();
+        let mut bg = palette.palette.neutral_7;
+        bg.alpha = 0.25;
+        let corner = palette.corner_radii;
+        let label_color = palette.palette.neutral_9;
+        match style {
+            TextInput::Default => Appearance {
+                background: Color::from(bg).into(),
+                border_radius: corner.radius_s.into(),
+                border_width: 1.0,
+                border_offset: None,
+                border_color: self.current_container().component.divider.into(),
+                icon_color: self.current_container().on.into(),
+                label_color: label_color.into(),
+            },
+            TextInput::ExpandableSearch => Appearance {
+                background: Color::TRANSPARENT.into(),
+                border_radius: corner.radius_xl.into(),
+                border_width: 0.0,
+                border_offset: None,
+                border_color: Color::TRANSPARENT,
+                icon_color: self.current_container().on.into(),
+                label_color: label_color.into(),
+            },
+            TextInput::Search => Appearance {
+                background: Color::from(bg).into(),
+                border_radius: corner.radius_xl.into(),
+                border_width: 0.0,
+                border_offset: None,
+                border_color: Color::TRANSPARENT,
+                icon_color: self.current_container().on.into(),
+                label_color: label_color.into(),
+            },
+            TextInput::Inline => Appearance {
+                background: Color::TRANSPARENT.into(),
+                border_radius: corner.radius_0.into(),
+                border_width: 0.0,
+                border_offset: None,
+                border_color: Color::TRANSPARENT,
+                icon_color: self.current_container().on.into(),
+                label_color: label_color.into(),
+            },
+        }
+    }
+
+    fn error(&self, style: &Self::Style) -> Appearance {
+        let palette = self.cosmic();
+        let mut bg = palette.palette.neutral_7;
+        bg.alpha = 0.25;
+        let corner = palette.corner_radii;
+        let label_color = palette.palette.neutral_9;
+
+        match style {
+            TextInput::Default => Appearance {
+                background: Color::from(bg).into(),
+                border_radius: corner.radius_s.into(),
+                border_width: 1.0,
+                border_offset: Some(2.0),
+                border_color: Color::from(palette.destructive_color()),
+                icon_color: self.current_container().on.into(),
+                label_color: label_color.into(),
+            },
+            TextInput::Search | TextInput::ExpandableSearch => Appearance {
+                background: Color::from(bg).into(),
+                border_radius: corner.radius_xl.into(),
+                border_width: 0.0,
+                border_offset: None,
+                border_color: Color::TRANSPARENT,
+                icon_color: self.current_container().on.into(),
+                label_color: label_color.into(),
+            },
+            TextInput::Inline => Appearance {
+                background: Color::TRANSPARENT.into(),
+                border_radius: corner.radius_0.into(),
+                border_width: 0.0,
+                border_offset: None,
+                border_color: Color::TRANSPARENT,
+                icon_color: self.current_container().on.into(),
+                label_color: label_color.into(),
+            },
+        }
+    }
+
+    fn hovered(&self, style: &Self::Style) -> Appearance {
+        let palette = self.cosmic();
+        let mut bg = palette.palette.neutral_7;
+        bg.alpha = 0.25;
+        let corner = palette.corner_radii;
+        let label_color = palette.palette.neutral_9;
+
+        match style {
+            TextInput::Default => Appearance {
+                background: Color::from(bg).into(),
+                border_radius: corner.radius_s.into(),
+                border_width: 1.0,
+                border_offset: None,
+                border_color: palette.accent.base.into(),
+                icon_color: self.current_container().on.into(),
+                label_color: label_color.into(),
+            },
+            TextInput::Search | TextInput::ExpandableSearch => Appearance {
+                background: Color::from(bg).into(),
+                border_radius: corner.radius_xl.into(),
+                border_offset: None,
+                border_width: 0.0,
+                border_color: Color::TRANSPARENT,
+                icon_color: self.current_container().on.into(),
+                label_color: label_color.into(),
+            },
+            TextInput::Inline => Appearance {
+                background: Color::from(self.current_container().component.hover).into(),
+                border_radius: corner.radius_0.into(),
+                border_width: 0.0,
+                border_offset: None,
+                border_color: Color::TRANSPARENT,
+                icon_color: self.current_container().on.into(),
+                label_color: label_color.into(),
+            },
+        }
+    }
+
+    fn focused(&self, style: &Self::Style) -> Appearance {
+        let palette = self.cosmic();
+        let mut bg = palette.palette.neutral_7;
+        bg.alpha = 0.25;
+        let corner = palette.corner_radii;
+        let label_color = palette.palette.neutral_9;
+
+        match style {
+            TextInput::Default => Appearance {
+                background: Color::from(bg).into(),
+                border_radius: corner.radius_s.into(),
+                border_width: 1.0,
+                border_offset: Some(2.0),
+                border_color: palette.accent.base.into(),
+                icon_color: self.current_container().on.into(),
+                label_color: label_color.into(),
+            },
+            TextInput::Search | TextInput::ExpandableSearch => Appearance {
+                background: Color::from(bg).into(),
+                border_radius: corner.radius_xl.into(),
+                border_width: 0.0,
+                border_offset: Some(2.0),
+                border_color: Color::TRANSPARENT,
+                icon_color: self.current_container().on.into(),
+                label_color: label_color.into(),
+            },
+            TextInput::Inline => Appearance {
+                background: Color::from(palette.accent.base).into(),
+                border_radius: corner.radius_0.into(),
+                border_width: 0.0,
+                border_offset: None,
+                border_color: Color::TRANSPARENT,
+                icon_color: palette.accent.on.into(),
+                label_color: label_color.into(),
+            },
+        }
+    }
+
+    fn placeholder_color(&self, _style: &Self::Style) -> Color {
+        let palette = self.cosmic();
+        let mut neutral_9 = palette.palette.neutral_9;
+        neutral_9.alpha = 0.7;
+        neutral_9.into()
+    }
+
+    fn value_color(&self, _style: &Self::Style) -> Color {
+        let palette = self.cosmic();
+
+        palette.palette.neutral_9.into()
+    }
+
+    fn selection_color(&self, _style: &Self::Style) -> Color {
+        let palette = self.cosmic();
+
+        palette.accent.base.into()
+    }
+
+    fn disabled_color(&self, _style: &Self::Style) -> Color {
+        let palette = self.cosmic();
+        let mut neutral_9 = palette.palette.neutral_9;
+        neutral_9.alpha = 0.5;
+        neutral_9.into()
+    }
+
+    fn disabled(&self, style: &Self::Style) -> Appearance {
+        self.active(style)
+    }
+}
