069163264fc88555a8f6f6542c383bb313082856
Michael Aaron Murphy <michael@mmurphy.dev>
2023-08-21 22:56:39 +0200

    chore(command): `future` does not need to restrict the message type

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -22,7 +22,7 @@ pub fn batch<M>(commands: impl IntoIterator<Item = Command<M>>) -> Command<M> {
 }
 
 /// Yields a command which will run the future on the runtime executor.
-pub fn future<M: Send + 'static>(future: impl Future<Output = M> + Send + 'static) -> Command<M> {
+pub fn future<M>(future: impl Future<Output = M> + Send + 'static) -> Command<M> {
     Command::single(Action::Future(Box::pin(future)))
 }
 
