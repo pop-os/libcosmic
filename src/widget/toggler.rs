2ddc8e3d2c3642329bfeb0bc89679db95116ea8d
Ashley Wulber <ashley@system76.com>
2024-02-02 19:46:19 -0500

    refactor: make Theme a generic parameter for the toggler
    This makes the toggler possible to use with custom themes in cosmic-time

--- src/widget/toggler.rs
+++ src/widget/toggler.rs
@@ -1,14 +1,17 @@
 // Copyright 2022 System76 <info@system76.com>
 // SPDX-License-Identifier: MPL-2.0
 
-use crate::Renderer;
 use iced::{widget, Length};
+use iced_core::text;
 
-pub fn toggler<'a, Message>(
+pub fn toggler<'a, Message, Theme: iced_widget::toggler::StyleSheet, Renderer>(
     label: impl Into<Option<String>>,
     is_checked: bool,
     f: impl Fn(bool) -> Message + 'a,
-) -> widget::Toggler<'a, Message, crate::Theme, Renderer> {
+) -> widget::Toggler<'a, Message, Theme, Renderer>
+where
+    Renderer: iced_core::Renderer + text::Renderer,
+{
     widget::Toggler::new(label, is_checked, f)
         .size(24)
         .spacing(12)
