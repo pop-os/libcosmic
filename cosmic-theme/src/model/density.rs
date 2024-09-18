0a34660a80ddb6a7f1ca1db0c5b60107be17bc72
Vukašin Vojinović <wscientia+github@gmail.com>
2024-09-18 20:09:25 +0200

    fix: move density to cosmic_theme

---
+++ cosmic-theme/src/model/density.rs
@@ -0,0 +1,65 @@
+use crate::Spacing;
+use serde::{Deserialize, Serialize};
+
+#[derive(Copy, Clone, Debug, Default, Eq, PartialEq, Serialize, Deserialize)]
+pub enum Density {
+    Compact,
+    Spacious,
+    #[default]
+    Standard,
+}
+
+impl From<Density> for Spacing {
+    fn from(value: Density) -> Self {
+        match value {
+            Density::Compact => Spacing {
+                space_none: 0,
+                space_xxxs: 4,
+                space_xxs: 4,
+                space_xs: 8,
+                space_s: 8,
+                space_m: 16,
+                space_l: 24,
+                space_xl: 32,
+                space_xxl: 48,
+                space_xxxl: 64,
+            },
+            Density::Spacious => Spacing {
+                space_none: 4,
+                space_xxxs: 8,
+                space_xxs: 12,
+                space_xs: 16,
+                space_s: 24,
+                space_m: 32,
+                space_l: 48,
+                space_xl: 64,
+                space_xxl: 128,
+                space_xxxl: 160,
+            },
+            Density::Standard => Spacing {
+                space_none: 0,
+                space_xxxs: 4,
+                space_xxs: 8,
+                space_xs: 12,
+                space_s: 16,
+                space_m: 24,
+                space_l: 32,
+                space_xl: 48,
+                space_xxl: 64,
+                space_xxxl: 128,
+            },
+        }
+    }
+}
+
+impl From<Spacing> for Density {
+    fn from(value: Spacing) -> Self {
+        if value.space_m.saturating_sub(16) == 0 {
+            Self::Compact
+        } else if value.space_m.saturating_sub(24) == 0 {
+            Self::Standard
+        } else {
+            Self::Spacious
+        }
+    }
+}
--- src/widget/header_bar.rs
+++ src/widget/header_bar.rs
@@ -1,7 +1,7 @@
 // Copyright 2022 System76 <info@system76.com>
 // SPDX-License-Identifier: MPL-2.0
 
-use crate::config::Density;
+use crate::cosmic_theme::Density;
 use crate::{ext::CollectionWidget, widget, Element};
 use apply::Apply;
 use derive_setters::Setters;
@@ -278,9 +278,9 @@ impl<'a, Message: Clone + 'static> HeaderBar<'a, Message> {
         end.push(self.window_controls());
 
         let (height, padding) = match self.density.unwrap_or_else(crate::config::header_size) {
-            crate::config::Density::Compact => (36.0, 2.0),
-            crate::config::Density::Standard => (48.0, 8.0),
-            crate::config::Density::Spacious => (48.0, 8.0),
+            Density::Compact => (36.0, 2.0),
+            Density::Spacious => (48.0, 8.0),
+            Density::Standard => (48.0, 8.0),
         };
 
         // Creates the headerbar widget.
