c904309151119d0bde581b62aae05ae069b24f63
Ashley Wulber <ashley@system76.com>
2024-10-18 12:10:45 -0400

    refactor(toggler): remove required on_toggle function

--- src/widget/toggler.rs
+++ src/widget/toggler.rs
@@ -6,13 +6,11 @@ use iced_core::text;
 
 pub fn toggler<'a, Message, Theme: iced_widget::toggler::Catalog, Renderer>(
     is_checked: bool,
-    f: impl Fn(bool) -> Message + 'a,
 ) -> widget::Toggler<'a, Message, Theme, Renderer>
 where
     Renderer: iced_core::Renderer + text::Renderer,
 {
     widget::Toggler::new(is_checked)
-        .on_toggle(f)
         .size(24)
         .width(Length::Shrink)
 }
