626ee929e4e561b642ffeec75085e07fe5614085
Michael Aaron Murphy <michael@mmurphy.dev>
2025-02-18 22:25:40 +0100

    improv: switch to Open Sans and Noto Sans Mono, with tweaked text styles

--- src/widget/text.rs
+++ src/widget/text.rs
@@ -27,48 +27,48 @@ pub enum Typography {
 /// [`Text`] widget with the Title 1 typography preset.
 pub fn title1<'a>(text: impl Into<Cow<'a, str>> + 'a) -> Text<'a, crate::Theme, Renderer> {
     Text::new(text.into())
-        .size(32.0)
-        .line_height(LineHeight::Absolute(44.0.into()))
-        .font(crate::font::light())
+        .size(35.0)
+        .line_height(LineHeight::Absolute(52.0.into()))
+        .font(crate::font::semibold())
 }
 
 /// [`Text`] widget with the Title 2 typography preset.
 pub fn title2<'a>(text: impl Into<Cow<'a, str>> + 'a) -> Text<'a, crate::Theme, Renderer> {
     Text::new(text.into())
-        .size(28.0)
-        .line_height(LineHeight::Absolute(36.0.into()))
-        .font(crate::font::default())
+        .size(29.0)
+        .line_height(LineHeight::Absolute(43.0.into()))
+        .font(crate::font::semibold())
 }
 
 /// [`Text`] widget with the Title 3 typography preset.
 pub fn title3<'a>(text: impl Into<Cow<'a, str>> + 'a) -> Text<'a, crate::Theme, Renderer> {
     Text::new(text.into())
         .size(24.0)
-        .line_height(LineHeight::Absolute(32.0.into()))
-        .font(crate::font::default())
+        .line_height(LineHeight::Absolute(36.0.into()))
+        .font(crate::font::bold())
 }
 
 /// [`Text`] widget with the Title 4 typography preset.
 pub fn title4<'a>(text: impl Into<Cow<'a, str>> + 'a) -> Text<'a, crate::Theme, Renderer> {
     Text::new(text.into())
         .size(20.0)
-        .line_height(LineHeight::Absolute(28.0.into()))
-        .font(crate::font::default())
+        .line_height(LineHeight::Absolute(30.0.into()))
+        .font(crate::font::bold())
 }
 
 /// [`Text`] widget with the Heading typography preset.
 pub fn heading<'a>(text: impl Into<Cow<'a, str>> + 'a) -> Text<'a, crate::Theme, Renderer> {
     Text::new(text.into())
         .size(14.0)
-        .line_height(LineHeight::Absolute(iced::Pixels(20.0)))
-        .font(crate::font::semibold())
+        .line_height(LineHeight::Absolute(iced::Pixels(21.0)))
+        .font(crate::font::bold())
 }
 
 /// [`Text`] widget with the Caption Heading typography preset.
 pub fn caption_heading<'a>(text: impl Into<Cow<'a, str>> + 'a) -> Text<'a, crate::Theme, Renderer> {
     Text::new(text.into())
-        .size(10.0)
-        .line_height(LineHeight::Absolute(iced::Pixels(14.0)))
+        .size(12.0)
+        .line_height(LineHeight::Absolute(iced::Pixels(17.0)))
         .font(crate::font::semibold())
 }
 
@@ -76,15 +76,15 @@ pub fn caption_heading<'a>(text: impl Into<Cow<'a, str>> + 'a) -> Text<'a, crate
 pub fn body<'a>(text: impl Into<Cow<'a, str>> + 'a) -> Text<'a, crate::Theme, Renderer> {
     Text::new(text.into())
         .size(14.0)
-        .line_height(LineHeight::Absolute(20.0.into()))
+        .line_height(LineHeight::Absolute(21.0.into()))
         .font(crate::font::default())
 }
 
 /// [`Text`] widget with the Caption typography preset.
 pub fn caption<'a>(text: impl Into<Cow<'a, str>> + 'a) -> Text<'a, crate::Theme, Renderer> {
     Text::new(text.into())
-        .size(10.0)
-        .line_height(LineHeight::Absolute(14.0.into()))
+        .size(12.0)
+        .line_height(LineHeight::Absolute(17.0.into()))
         .font(crate::font::default())
 }
 
