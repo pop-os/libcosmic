b6a84b8c8131820501132dbf7f55fc9190147483
mmstick <mmstick@users.noreply.github.com>
2025-02-20 12:40:10 +0000

    deploy: 8a0e74b189f53dae9b8001c6fa5cf1820b3ececb

---
+++ trait.impl/raw_window_handle/borrowed/trait.HasWindowHandle.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["cosmic",[]],["iced",[]],["raw_window_handle",[]],["softbuffer",[["impl <a class=\"trait\" href=\"raw_window_handle/borrowed/trait.HasWindowHandle.html\" title=\"trait raw_window_handle::borrowed::HasWindowHandle\">HasWindowHandle</a> for <a class=\"struct\" href=\"softbuffer/struct.NoWindowHandle.html\" title=\"struct softbuffer::NoWindowHandle\">NoWindowHandle</a>"],["impl&lt;D: <a class=\"trait\" href=\"raw_window_handle/borrowed/trait.HasDisplayHandle.html\" title=\"trait raw_window_handle::borrowed::HasDisplayHandle\">HasDisplayHandle</a>, W: <a class=\"trait\" href=\"raw_window_handle/borrowed/trait.HasWindowHandle.html\" title=\"trait raw_window_handle::borrowed::HasWindowHandle\">HasWindowHandle</a>&gt; <a class=\"trait\" href=\"raw_window_handle/borrowed/trait.HasWindowHandle.html\" title=\"trait raw_window_handle::borrowed::HasWindowHandle\">HasWindowHandle</a> for <a class=\"struct\" href=\"softbuffer/struct.Surface.html\" title=\"struct softbuffer::Surface\">Surface</a>&lt;D, W&gt;"]]],["winit",[["impl <a class=\"trait\" href=\"raw_window_handle/borrowed/trait.HasWindowHandle.html\" title=\"trait raw_window_handle::borrowed::HasWindowHandle\">HasWindowHandle</a> for dyn <a class=\"trait\" href=\"winit/window/trait.Window.html\" title=\"trait winit::window::Window\">Window</a> + '_"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[13,12,25,963,305]}
