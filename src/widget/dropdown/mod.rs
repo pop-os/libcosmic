e6556d83f53b34c1fc76088cfdecbbf392259578
Ashley Wulber <ashley@system76.com>
2025-03-07 13:14:48 -0500

    refactor: more ergonomic dropdown popups

--- src/widget/dropdown/mod.rs
+++ src/widget/dropdown/mod.rs
@@ -5,12 +5,15 @@
 //! Displays a list of options in a popover menu on select.
 
 pub mod menu;
+use iced_core::window;
 pub use menu::Menu;
 
 pub mod multi;
 
 mod widget;
 pub use widget::*;
+
+use crate::surface;
 
 /// Displays a list of options in a popover menu on select.
 pub fn dropdown<
@@ -20,6 +23,29 @@ pub fn dropdown<
     selections: &[S],
     selected: Option<usize>,
     on_selected: impl Fn(usize) -> Message + Send + Sync + 'static,
-) -> Dropdown<'_, S, Message> {
-    Dropdown::new(selections, selected, on_selected)
+) -> Dropdown<'_, S, Message, Message> {
+    Dropdown::new(selections, selected, move |_, i| on_selected(i))
+}
+
+#[cfg(all(feature = "winit", feature = "wayland"))]
+/// Displays a list of options in a popover menu on select.
+/// AppMessage must be the App's toplevel message.
+pub fn popup_dropdown<
+    'a,
+    S: AsRef<str> + std::clone::Clone + Send + Sync + 'static,
+    Message: 'static + Clone,
+    AppMessage: 'static + Clone,
+>(
+    selections: &'a [S],
+    selected: Option<usize>,
+    on_selected: impl Fn(window::Id, usize) -> Message + Send + Sync + 'static,
+    parent_id: window::Id,
+    on_surface_action: impl Fn(surface::Action) -> Message + 'a,
+    map_action: impl Fn(Message) -> AppMessage + 'static + Send + Sync,
+) -> Dropdown<'a, S, Message, AppMessage> {
+    Dropdown::new(selections, selected, on_selected).with_popup(
+        parent_id,
+        on_surface_action,
+        map_action,
+    )
 }
