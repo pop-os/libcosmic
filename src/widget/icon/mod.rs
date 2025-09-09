39a5607400452fbf27fe2c1d14c1d2dea8d51447
Vukašin Vojinović <150025636+git-f0x@users.noreply.github.com>
2025-09-09 15:30:12 +0200

    improv(icon): use correct size variant for `Named`
    Update`Icon::size` method to correctly handle `Named` icons by using the provided size retroactively.

--- src/widget/icon/mod.rs
+++ src/widget/icon/mod.rs
@@ -43,6 +43,7 @@ pub struct Icon {
     #[setters(skip)]
     handle: Handle,
     class: crate::theme::Svg,
+    #[setters(skip)]
     pub(super) size: u16,
     content_fit: ContentFit,
     #[setters(strip_option)]
@@ -70,6 +71,22 @@ impl Icon {
         }
 
         None
+    }
+
+    #[must_use]
+    pub fn size(mut self, size: u16) -> Self {
+        match &self.handle.data {
+            // ensures correct icon size variant selection
+            Data::Name(named) => {
+                let mut new_named = named.clone();
+                new_named.size = Some(size);
+                self.handle = new_named.handle();
+            }
+            _ => {
+                self.size = size;
+            }
+        }
+        self
     }
 
     #[must_use]
