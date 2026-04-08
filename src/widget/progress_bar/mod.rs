af20ecc83375fc62e462463cffca0bbd6457d907
Ashley Wulber <ashley@system76.com>
2026-04-07 22:29:53 -0400

    cleanup

--- src/widget/progress_bar/mod.rs
+++ src/widget/progress_bar/mod.rs
@@ -3,21 +3,21 @@ pub mod linear;
 pub mod style;
 
 /// A spinner / throbber widget that can be used to indicate that some operation is in progress.
-pub fn indeterminate_circular<Message>() -> circular::Circular<crate::Theme> {
+pub fn indeterminate_circular() -> circular::Circular<crate::Theme> {
     circular::Circular::new()
 }
 
 /// A linear throbber widget that can be used to indicate that some operation is in progress.
-pub fn indeterminate_linear<Message>() -> linear::Linear<crate::Theme> {
+pub fn indeterminate_linear() -> linear::Linear<crate::Theme> {
     linear::Linear::new()
 }
 
 /// A circular progress spinner widget that can be used to indicate the progress of some operation.
-pub fn determinate_circular<Message>(progress: f32) -> circular::Circular<crate::Theme> {
+pub fn determinate_circular(progress: f32) -> circular::Circular<crate::Theme> {
     circular::Circular::new().progress(progress)
 }
 
 /// A linear progress bar widget that can be used to indicate the progress of some operation.
-pub fn determinate_linear<Message>(progress: f32) -> linear::Linear<crate::Theme> {
+pub fn determinate_linear(progress: f32) -> linear::Linear<crate::Theme> {
     linear::Linear::new().progress(progress)
 }
