83f0539ea1c1b391e69b162ad7287be72bcb5569
Ashley Wulber <ashley@system76.com>
2026-07-29 12:09:27 -0400

    fix(menu): path highlight corners flipped

--- src/widget/menu/menu_inner.rs
+++ src/widget/menu/menu_inner.rs
@@ -703,7 +703,7 @@ impl<'b, Message: Clone + 'static> Menu<'b, Message> {
             }
 
             _ => {}
-        };
+        }
         None
     }
 
@@ -811,11 +811,11 @@ impl<'b, Message: Clone + 'static> Menu<'b, Message> {
                                 let rad_0 = theme.cosmic().radius_0();
                                 if start_index != end_index {
                                     if 0 == i {
-                                        rad[0] = rad_0[0];
-                                        rad[1] = rad_0[1];
+                                        rad[2] = rad_0[0];
+                                        rad[3] = rad_0[1];
                                     } else if i == end_index - start_index {
-                                        rad[2] = rad_0[2];
-                                        rad[3] = rad_0[3];
+                                        rad[0] = rad_0[2];
+                                        rad[1] = rad_0[3];
                                     } else {
                                         rad = rad_0;
                                     }
