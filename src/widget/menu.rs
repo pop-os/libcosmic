aac833d37d6090bf9bee3b53c5c379af088e2508
Hojjat <hojjat.abdollahi@gmail.com>
2026-09-02 19:03:07 -0600

    feat(menu): add Item::Entry with a reservable icon slot

--- src/widget/menu.rs
+++ src/widget/menu.rs
@@ -71,8 +71,8 @@ pub mod menu_column;
 mod menu_inner;
 mod menu_tree;
 pub use menu_tree::{
-    MenuItem as Item, MenuTree as Tree, menu_button, menu_items as items, menu_root as root,
-    nav_context,
+    Entry, IconSlot, MenuItem as Item, MenuTree as Tree, menu_button, menu_items as items,
+    menu_root as root, nav_context,
 };
 
 pub use crate::style::menu_bar::{Appearance, StyleSheet};
