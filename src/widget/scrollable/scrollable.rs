eaa7a5a24c4359ebeaab507855388942e8984d5d
Vukašin Vojinović <150025636+git-f0x@users.noreply.github.com>
2026-09-18 21:04:54 +0200

    chore: update iced
    Adapts to scrollable changes

--- src/widget/scrollable/scrollable.rs
+++ src/widget/scrollable/scrollable.rs
@@ -2,30 +2,28 @@
 // SPDX-License-Identifier: MPL-2.0
 
 use crate::{Element, Renderer};
-use iced::widget;
+use iced::widget::scrollable::{Direction, Scrollable, Scrollbar};
 
 pub fn scrollable<'a, Message>(
     element: impl Into<Element<'a, Message>>,
-) -> widget::Scrollable<'a, Message, crate::Theme, Renderer> {
+) -> Scrollable<'a, Message, crate::Theme, Renderer> {
     vertical(element)
 }
 
 pub fn vertical<'a, Message>(
     element: impl Into<Element<'a, Message>>,
-) -> widget::Scrollable<'a, Message, crate::Theme, Renderer> {
-    widget::scrollable(element)
-        .scroller_width(8.0)
-        .scrollbar_width(8.0)
-        .scrollbar_padding(8.0)
+) -> Scrollable<'a, Message, crate::Theme, Renderer> {
+    iced::widget::scrollable(element)
+        .padding(8.0)
+        .direction(Direction::Vertical(
+            Scrollbar::new().width(8.0).scroller_width(8.0),
+        ))
 }
 
 pub fn horizontal<'a, Message>(
     element: impl Into<Element<'a, Message>>,
-) -> widget::Scrollable<'a, Message, crate::Theme, Renderer> {
-    widget::scrollable(element)
-        .direction(widget::scrollable::Direction::Horizontal(
-            widget::scrollable::Scrollbar::new(),
-        ))
-        .scroller_width(8.0)
-        .scrollbar_width(8.0)
+) -> Scrollable<'a, Message, crate::Theme, Renderer> {
+    iced::widget::scrollable(element).direction(Direction::Horizontal(
+        Scrollbar::new().width(8.0).scroller_width(8.0),
+    ))
 }
